58769b1dd791d7636b7c4cd43407dcc157343b20
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-11-29 22:50:32 +0530

    Add domain cleaner

--- src/helper.js
+++ src/helper.js
@@ -2,6 +2,25 @@ const waitFor = async (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
+const domainCleaner = (url) => {
+    // Regular expression to check if the URL starts with https://www.instagram.com/
+    const instagramRegex = /^https:\/\/www\.instagram\.com\//i;
+
+    try {
+        // Check if the URL is a valid Instagram URL
+        if (!instagramRegex.test(url)) {
+            throw new Error("Share Instagram Post, Reels, etc.");
+        }
+
+        // Return success true and the clean URL
+        return { success: true, data: url };
+    } catch (error) {
+        // Return success false and the error message
+        return { success: false, data: error.message };
+    }
+}
+
 module.exports = {
-    waitFor
+    waitFor,
+    domainCleaner
 };
