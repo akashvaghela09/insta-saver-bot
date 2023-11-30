b2f834e8547eb740364196adaec7ef9c9931ec8e
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2023-11-30 17:36:34 +0530

    Update port message

--- src/index.js
+++ src/index.js
@@ -94,5 +94,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
