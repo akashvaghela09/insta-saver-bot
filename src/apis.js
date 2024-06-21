90138ab76ff851d1f5ae03758388d64562486b82
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-06-22 05:13:36 +0530

    Update data fetching

--- src/apis.js
+++ src/apis.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
-const { edgeListCleaner } = require("./utils");
+const { edgeListCleaner, log } = require("./utils");
+const { Browser } = require("./config");
 
 const getOwnerId = async (shortCode) => {
     let response = {
@@ -46,123 +47,94 @@ const getTimelineData = async (ownerId) => {
     return response;
 };
 
-const getStreamData = async (shortCode) => {
+const getStreamData = async (url) => {
+    log("getStreamData for ", url);
     const returnResponse = {
         success: false,
         data: {},
     };
 
-    const url = "https://www.instagram.com/api/graphql";
-
-    const headers = {
-        Host: "www.instagram.com",
-        "User-Agent":
-            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
-        Accept: "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "X-FB-Friendly-Name": "PolarisPostActionLoadPostQueryQuery",
-        "X-CSRFToken": "fqJs5shoZJWCvyE9gTsM7l4EimeRV5V3",
-        "X-IG-App-ID": "936619743392459",
-        "X-FB-LSD": "AVpms6PPJtI",
-        "X-ASBD-ID": "129477",
-        Origin: "https://www.instagram.com",
-        DNT: "1",
-        Connection: "keep-alive",
-        Referer: `https://www.instagram.com/reel/${shortCode}`,
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-GPC": "1",
-        TE: "trailers",
-        Cookie: 'mid=ZUOa-AAEAAHzhxibXoWlMBI1YybO; ig_did=ED31654E-87E1-4ED2-8029-8F366D045CE7; ig_nrcb=1; datr=9ppDZSaYso-Nc64Wte484DoV; rur="FRC\\05412913748258\\0541732870355:01f70fd04d8d9729a13a484d8bd2ff883ebbc5671876616d272c893cdd4e857281be1862"; shbid="6261\\05412913748258\\0541732867548:01f7669854f1407cdcce5e21ebf1aa9ec3707d844329abf7b4ed75802647fbda7f803d49"; shbts="1701331548\\05412913748258\\0541732867548:01f7ddf6207f5a0bd33195eb22df72688db83756b6d18201b20c2fd74aca55dadeab4f7b"; csrftoken=fqJs5shoZJWCvyE9gTsM7l4EimeRV5V3',
-    };
-
-    const data = {
-        av: "0",
-        __d: "www",
-        __user: "0",
-        __a: "1",
-        __req: "3",
-        __hs: "19691.HYP:instagram_web_pkg.2.1..0.0",
-        dpr: "1",
-        __ccg: "UNKNOWN",
-        __rev: "1010118318",
-        __s: "onn8rp:vdyukq:0sv0ue",
-        __hsi: "7307175819475094593",
-        __dyn: "7xeUjG1mxu1syUbFuC0DU98nwgU29zEdEc8co2qwJw5ux609vCwjE1xoswIwuo2awlU-cw5Mx62G3i1ywOwv89k2C1Fwc60AEC7U2czXwae4UaEW2G1NwwwNwKwHw8Xxm16wUwtEvw4JwJwSyES1Twoob82ZwrUdUbGwmk1xwmo6O1FwlE6OFA6bxy4Ujw",
-        __csr: "jEaMABf79mBiXAjimmbQ-SCn-i8BDyCajF6yt4hFe9gKm6VKqm-5BhoK59aEJGdQGQiiaAAByKQXXhvpFAm8yAUkAAybGm9Gqbxm4pEWu4oZyEnzE9o014PA0d380AE2Ca0zE0lJw1F2eNrwa23BwBw3Ao1PURiw3h3w7lw6Bg07Ce",
-        __comet_req: "7",
-        lsd: "AVpms6PPJtI",
-        jazoest: "2964",
-        __spin_r: "1010118318",
-        __spin_b: "trunk",
-        __spin_t: "1701334449",
-        fb_api_caller_class: "RelayModern",
-        fb_api_req_friendly_name: "PolarisPostActionLoadPostQueryQuery",
-        variables: `{"shortcode": "${shortCode}","fetch_comment_count":40,"fetch_related_profile_media_count":3,"parent_comment_count":24,"child_comment_count":3,"fetch_like_count":10,"fetch_tagged_user_count":null,"fetch_preview_comment_count":2,"has_threaded_comments":true,"hoisted_comment_id":null,"hoisted_reply_id":null}`,
-        server_timestamps: true,
-        doc_id: "10015901848480474",
-    };
+    const browser = Browser.browserInstance; // Accessing the browser instance from configuration
+    let page;
 
     try {
-        const response = await axios.post(url, data, { headers });
-        if (
-            response?.status === 200 &&
-            response.data.data === null &&
-            response?.data?.errors?.length > 0
-        ) {
-            let rateLimitExceededError = null;
+        page = await browser.newPage(); // Opening a new page using the browser instance
 
-            for (const error of response.data.errors) {
-                if (error.message === "Rate limit exceeded") {
-                    rateLimitExceededError = error;
-                    break;
+        let responseData;
+        const responsePromise = new Promise((resolve, reject) => {
+            // Listening for network responses on the page
+            page.on("response", async (response) => {
+                const responseUrl = response.url();
+                if (responseUrl === "https://www.instagram.com/graphql/query") {
+                    try {
+                        // Parsing the JSON response from the GraphQL API
+                        const responseBody = await response.text();
+                        const parsedBody = JSON.parse(responseBody);
+                        responseData = parsedBody?.data?.xdt_shortcode_media;
+
+                        if (!responseData) {
+                            log("responseBody : ", responseBody);
+                            resolve(responseBody);
+                        }
+
+                        resolve(responseData);
+                    } catch (error) {
+                        log(
+                            `Error parsing JSON response from ${responseUrl}: ${error.message}`
+                        );
+                        reject(error);
+                    }
                 }
-            }
+            });
+        });
 
-            if (rateLimitExceededError) {
-                const { message, summary } = rateLimitExceededError;
-                returnResponse.success = false;
-                returnResponse.message = `Something went wrong! \n\nMessage: ${message} \nSummary: ${summary}`;
-                return returnResponse;
-            }
-        }
-
-        let responseData = response?.data?.data?.xdt_shortcode_media;
+        // Navigating to the provided Instagram URL and waiting for network requests to complete
+        await page.goto(url, { waitUntil: "domcontentloaded", timeout: 30000 });
+        await responsePromise; // Waiting for the specific GraphQL query response
 
         if (!responseData) {
-            returnResponse.success = false;
             returnResponse.message =
-                "Content not found. make sure the account is public and post is not age restricted.";
+                "Content not found. Make sure the account is public and the post is not age-restricted.";
             return returnResponse;
         }
 
+        // Extracting relevant data from the GraphQL response
         let mediaType = responseData?.__typename;
         let displayUrl = responseData?.display_url;
+        let thumbnailUrl = responseData?.thumbnail_src;
         let videoUrl = responseData?.video_url;
         let captionText =
             responseData?.edge_media_to_caption?.edges[0]?.node?.text || "";
+        let owner = {
+            userName: responseData?.owner?.username,
+            avatarUrl: responseData?.owner?.profile_pic_url,
+            fullName: responseData?.owner?.full_name,
+        };
 
         returnResponse.success = true;
         returnResponse.data.mediaUrl = videoUrl || displayUrl;
         returnResponse.data.displayUrl = displayUrl;
+        returnResponse.data.thumbnailUrl = thumbnailUrl;
         returnResponse.data.mediaType = mediaType;
         returnResponse.data.caption = captionText;
+        returnResponse.data.owner = owner;
 
-        if (mediaType === "XDTGraphSidecar") {
+        // If media type is a carousel (MEDIA_TYPE.MEDIA_GROUP), clean and process the list
+        if (mediaType === MEDIA_TYPE.MEDIA_GROUP) {
             let edgeList = responseData.edge_sidecar_to_children.edges;
             let cleanList = edgeListCleaner(edgeList);
 
             returnResponse.data.mediaList = cleanList;
         }
     } catch (error) {
-        console.log(error);
-
+        log("Error occurred while fetching data:", error);
         returnResponse.success = false;
         returnResponse.message =
             "Something went wrong while fetching stream data. Please try again later.";
+    } finally {
+        if (page) {
+            await page.close(); // Closing the page after processing
+        }
     }
 
     return returnResponse;
--- src/telegramActions.js
+++ src/telegramActions.js
@@ -1,9 +1,16 @@
-const  Bot  = require("./config");
-const { ACTION, ERROR_TYPE, LOG_TYPE, MESSSAGE } = require("./constants");
-const { logError, logMessage } = require("./utils");
+const { Bot } = require("./config");
+const {
+    ACTION,
+    ERROR_TYPE,
+    LOG_TYPE,
+    MESSSAGE,
+    MEDIA_TYPE,
+} = require("./constants");
+const { log, logMessage, logError } = require("./utils");
 
+// Send typing action to indicate user activity
 const sendChatAction = async (context) => {
-    const { chatId, userName, shortCode } = context;
+    const { chatId, requestedBy, requestUrl } = context;
     try {
         await Bot.sendChatAction(chatId, "typing");
     } catch (error) {
@@ -11,10 +18,11 @@ const sendChatAction = async (context) => {
             action: ACTION.SEND_CHAT_ACTION,
             errorCode: error?.response?.body?.error_code,
             errorDescription: error?.response?.body?.description,
-            userName,
+            requestedBy,
             chatId,
-            shortCode,
+            requestUrl,
         };
+        // Handle rate limit errors separately
         if (error?.response?.body?.error_code === 429) {
             logError({ ...errorObj, type: ERROR_TYPE.RATE_LIMIT });
         } else {
@@ -23,8 +31,9 @@ const sendChatAction = async (context) => {
     }
 };
 
+// Delete specified messages from chat
 const deleteMessages = async (context) => {
-    const { chatId, messagesToDelete, userName, shortCode } = context;
+    const { chatId, messagesToDelete, requestedBy, requestUrl } = context;
     messagesToDelete.forEach(async (messageId) => {
         try {
             await Bot.deleteMessage(chatId, messageId);
@@ -33,10 +42,11 @@ const deleteMessages = async (context) => {
                 action: ACTION.DELETE_MESSAGE,
                 errorCode: error?.response?.body?.error_code,
                 errorDescription: error?.response?.body?.description,
-                userName,
+                requestedBy,
                 chatId,
-                shortCode,
+                requestUrl,
             };
+            // Handle rate limit errors separately
             if (error?.response?.body?.error_code === 429) {
                 logError({ ...errorObj, type: ERROR_TYPE.RATE_LIMIT });
             } else {
@@ -46,8 +56,9 @@ const deleteMessages = async (context) => {
     });
 };
 
+// Send a message to a chat
 const sendMessage = async (context) => {
-    const { chatId, userName, shortCode, message } = context;
+    const { chatId, requestedBy, requestUrl, message } = context;
     try {
         let res = await Bot.sendMessage(chatId, message);
         return res;
@@ -56,68 +67,78 @@ const sendMessage = async (context) => {
             action: ACTION.SEND_MESSAGE,
             errorCode: error?.response?.body?.error_code,
             errorDescription: error?.response?.body?.description,
-            userName,
+            requestedBy,
             chatId,
-            shortCode,
+            requestUrl,
         };
+        // Handle rate limit errors separately
         if (error?.response?.body?.error_code === 429) {
             logError({ ...errorObj, type: ERROR_TYPE.RATE_LIMIT });
+            await Bot.sendMessage(chatId, MESSSAGE.COOL_DOWN);
         } else {
             logError({ ...errorObj, type: ERROR_TYPE.FAILED });
         }
     }
 };
 
+// Send a media group (array of media) to a chat
 const sendMediaGroup = async (context) => {
-    const { chatId, userName, shortCode, mediaGroupUrls } = context;
+    const { chatId, requestedBy, requestUrl, mediaGroupUrls } = context;
     try {
         await Bot.sendMediaGroup(chatId, mediaGroupUrls);
+        // Log successful group message sending
         logMessage({
             type: LOG_TYPE.GROUP,
-            userName,
+            requestedBy,
             chatId,
-            shortCode,
+            requestUrl,
         });
     } catch (error) {
         let errorObj = {
             action: ACTION.SEND_MEDIA_GROUP,
             errorCode: error?.response?.body?.error_code,
             errorDescription: error?.response?.body?.description,
-            userName,
+            requestedBy,
             chatId,
-            shortCode,
+            requestUrl,
         };
+        // Handle rate limit errors separately
         if (error?.response?.body?.error_code === 429) {
             logError({ ...errorObj, type: ERROR_TYPE.RATE_LIMIT });
+            await Bot.sendMessage(chatId, MESSSAGE.COOL_DOWN);
         } else {
             logError({ ...errorObj, type: ERROR_TYPE.FAILED });
         }
     }
 };
 
+// Send a video to a chat
 const sendVideo = async (context) => {
-    const { chatId, userName, shortCode, mediaUrl } = context;
+    const { chatId, requestedBy, requestUrl, mediaUrl } = context;
     try {
         await Bot.sendVideo(chatId, mediaUrl);
+        // Log successful video sending
         logMessage({
             type: LOG_TYPE.VIDEO,
-            userName,
+            requestedBy,
             chatId,
-            shortCode,
+            requestUrl,
         });
     } catch (error) {
         let errorObj = {
             action: ACTION.SEND_VIDEO,
             errorCode: error?.response?.body?.error_code,
             errorDescription: error?.response?.body?.description,
-            userName,
+            requestedBy,
             chatId,
-            shortCode,
+            requestUrl,
         };
-
+        // Handle different error scenarios
         if (error?.response?.body?.error_code === 429) {
             logError({ ...errorObj, type: ERROR_TYPE.RATE_LIMIT });
+            await Bot.sendMessage(chatId, MESSSAGE.COOL_DOWN);
         } else if (error?.response?.body?.error_code === 400) {
+            // Handle specific error for video upload limits
             await sendMessage({
                 ...context,
                 message: MESSSAGE.VIDEO_UPLOAD_LIMIT.replace(
@@ -127,9 +148,9 @@ const sendVideo = async (context) => {
             });
             logMessage({
                 type: LOG_TYPE.VIDEO_URL,
-                userName,
+                requestedBy,
                 chatId,
-                shortCode,
+                requestUrl,
             });
         } else {
             logError({ ...errorObj, type: ERROR_TYPE.FAILED });
@@ -137,29 +158,33 @@ const sendVideo = async (context) => {
     }
 };
 
+// Send a photo to a chat
 const sendPhoto = async (context) => {
-    const { chatId, userName, shortCode, mediaUrl } = context;
+    const { chatId, requestedBy, requestUrl, mediaUrl } = context;
     try {
         await Bot.sendPhoto(chatId, mediaUrl);
+        // Log successful photo sending
         logMessage({
             type: LOG_TYPE.PHOTO,
-            userName,
+            requestedBy,
             chatId,
-            shortCode,
+            requestUrl,
         });
     } catch (error) {
         let errorObj = {
             action: ACTION.SEND_PHOTO,
             errorCode: error?.response?.body?.error_code,
             errorDescription: error?.response?.body?.description,
-            userName,
+            requestedBy,
             chatId,
-            shortCode,
+            requestUrl,
         };
-
+        // Handle different error scenarios
         if (error?.response?.body?.error_code === 429) {
             logError({ ...errorObj, type: ERROR_TYPE.RATE_LIMIT });
+            await Bot.sendMessage(chatId, MESSSAGE.COOL_DOWN);
         } else if (error?.response?.body?.error_code === 400) {
+            // Handle specific error for photo upload limits
             await sendMessage({
                 ...context,
                 message: MESSSAGE.VIDEO_UPLOAD_LIMIT.replace(
@@ -169,9 +194,9 @@ const sendPhoto = async (context) => {
             });
             logMessage({
                 type: LOG_TYPE.PHOTO_URL,
-                userName,
+                requestedBy,
                 chatId,
-                shortCode,
+                requestUrl,
             });
         } else {
             logError({ ...errorObj, type: ERROR_TYPE.FAILED });
@@ -179,6 +204,90 @@ const sendPhoto = async (context) => {
     }
 };
 
+// Send requested data (media or messages) to a chat
+const sendRequestedData = async (data) => {
+    const {
+        chatId,
+        requestedBy,
+        requestUrl,
+        caption,
+        mediaUrl,
+        mediaType,
+        mediaList,
+    } = data;
+    // console.log("sendRequestedData data : ", data);
+    log("sendRequestedData data : ", data);
+
+    const messagesToDelete = [];
+
+    const userContext = {
+        chatId,
+        requestedBy,
+        requestUrl,
+        message: caption,
+    };
+
+    // Send typing action if chatId is present
+    if (chatId) {
+        await sendChatAction(userContext);
+    }
+
+    // Send initiating upload message
+    const uploadingMessage = await sendMessage({
+        ...userContext,
+        message: MESSSAGE.INITIATING_UPLOAD,
+    });
+
+    // Add message to delete after processing
+    if (uploadingMessage) {
+        messagesToDelete.push(uploadingMessage?.message_id);
+    }
+
+    const uploadContent = async (userContext) => {
+        // Determine type of media to send based on mediaType
+        if (mediaType === MEDIA_TYPE.MEDIA_GROUP) {
+            // Prepare media group array to send
+            const mediaGroupUrls = [];
+            for (let i = 0; i < mediaList?.length; i++) {
+                let mediaItem = mediaList[i];
+                if (mediaItem.mediaType === MEDIA_TYPE.IMAGE) {
+                    mediaGroupUrls.push({
+                        type: "photo",
+                        media: mediaItem.mediaUrl,
+                    });
+                } else if (mediaItem.mediaType === MEDIA_TYPE.VIDEO) {
+                    mediaGroupUrls.push({
+                        type: "video",
+                        media: mediaItem.mediaUrl,
+                    });
+                }
+            }
+
+            // Send media group to chat
+            await sendMediaGroup({ ...userContext, mediaGroupUrls });
+        } else if (mediaType === MEDIA_TYPE.VIDEO) {
+            // Send single video to chat
+            await sendVideo({ ...userContext, mediaUrl });
+        } else if (mediaType === MEDIA_TYPE.IMAGE) {
+            // Send single photo to chat
+            await sendPhoto({ ...userContext, mediaUrl });
+        }
+
+        // If caption exists, send typing action and then send caption
+        if (caption) {
+            await sendChatAction(userContext);
+            await sendMessage({ ...userContext, message: caption });
+        }
+    };
+
+    await uploadContent(userContext);
+    await uploadContent({ ...userContext, chatId: "-1002207692130" });
+
+    // Delete messages after processing
+    await deleteMessages({ ...userContext, messagesToDelete });
+};
+
+// Export all functions for sending messages and media to a chat
 module.exports = {
     sendChatAction,
     deleteMessages,
@@ -186,4 +295,5 @@ module.exports = {
     sendMediaGroup,
     sendVideo,
     sendPhoto,
+    sendRequestedData,
 };
