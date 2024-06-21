5c2fc18f00416e17c7d91e622acd00dad25bf68c
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-06-22 02:34:57 +0530

    Add browser methods

--- src/config/browser.js
+++ src/config/browser.js
@@ -1,6 +1,10 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
 const { log } = require("../utils");
-const browserPath = process.env.PUPPETEER_EXECUTABLE_PATH;
+
+const userDataDir = path.join(__dirname, "puppeteer_data");
+const cookiesFilePath = path.join(__dirname, "cookies.json");
 
 class Browser {
     static browserInstance = null;
@@ -10,26 +14,21 @@ class Browser {
             try {
                 const launchOptions = {
                     headless: true,
+                    defaultViewport: { width: 1280, height: 800 },
                     args: [
                         "--no-sandbox",
-                        "--window-size=1920,1080",
-                        "--ignore-certificate-errors",
-                        "--ignore-certificate-errors-spki-list",
+                        "--disable-setuid-sandbox",
                         "--disable-dev-shm-usage",
                     ],
-                    timeout: 0,
+                    userDataDir,
                 };
 
-                if (browserPath) {
-                    launchOptions.executablePath = browserPath;
-                }
-
-                this.browserInstance = await puppeteer.launch(
-                    browserPath ? launchOptions : {}
-                );
+                this.browserInstance = await puppeteer.launch(launchOptions);
                 log("Browser launched successfully");
+
+                // Check authentication status on launch
+                await this.CheckAuth();
             } catch (error) {
-                console.log("Error launching browser:", error);
                 log("Error launching browser:", error);
             }
         }
@@ -41,8 +40,87 @@ class Browser {
         if (this.browserInstance) {
             await this.browserInstance.close();
             this.browserInstance = null;
-            console.log("Browser closed successfully");
             log("Browser closed successfully");
+        }
+    }
+
+    static async Login(page) {
+        try {
+            log("Tring to log in ...");
+            // Navigate to Instagram login page
+            await page.goto("https://www.instagram.com/accounts/login/", {
+                waitUntil: "networkidle2",
+            });
+
+            // Increase default navigation timeout
+            await page.setDefaultNavigationTimeout(60000); // 60 seconds
+
+            // Enter username and password and click login button
+            await page.type('[name="username"]', process.env.INSTA_USERNAME);
+            await page.type('[name="password"]', process.env.INSTA_PASSWORD);
+            await page.click('[type="submit"]');
+
+            // Wait for navigation to complete after login
+            await page.waitForNavigation({ waitUntil: "networkidle2" });
+
+            log("Logged in successfully");
+
+            await this.CheckAuth();
+
+            // Save cookies after successful login
+            const cookies = await page.cookies();
+            await fs.promises.writeFile(
+                cookiesFilePath,
+                JSON.stringify(cookies, null, 2)
+            );
+            log("Cookies saved");
+        } catch (error) {
+            log("Failed to login bot account:", error);
+        }
+    }
+
+    static async CheckAuth() {
+        log("Checking auth status");
+        const page = await this.browserInstance.newPage();
+        try {
+            await this.restoreCookies(page); // Restore cookies
+
+            // Navigate to a protected page to check if authenticated
+            await page.goto("https://www.instagram.com/accounts/login/", {
+                waitUntil: "networkidle2",
+            });
+
+            // Check if redirected to Instagram home page after login
+            const redirected = page.url() === "https://www.instagram.com/";
+
+            await page.setViewport({ width: 1280, height: 800 });
+
+            if (redirected) {
+                this.authStatus = true;
+                log("Authentication status: Authenticated");
+            } else {
+                // Not authenticated
+                this.authStatus = false;
+                log("Authentication status: Not Authenticated");
+                await this.Login(page); // Perform login if not authenticated
+            }
+        } catch (error) {
+            log("Error checking authentication:", error);
+        } finally {
+            await page.close();
+            log("Page closed after checking auth");
+        }
+    }
+
+    static async restoreCookies(page) {
+        if (fs.existsSync(cookiesFilePath)) {
+            const cookies = JSON.parse(
+                await fs.promises.readFile(cookiesFilePath, "utf8")
+            );
+            if (cookies.length !== 0) {
+                await page.setCookie(...cookies);
+                log("Cookies restored");
+            }
         }
     }
 }
