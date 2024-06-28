553e8ceaf0fb47e3d0755d951b5ca6ade3f57f9c
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-06-28 19:48:50 +0530

    Check for valid instagram url

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,5 @@
+const { log } = require("./logs");
+
 const waitFor = async (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
 };
@@ -6,7 +8,39 @@ const findMediaByShortCode = (mediaList, shortCode) => {
     return mediaList.find((media) => media.shortCode === shortCode) || null;
 };
 
+const isValidInstaUrl = (url) => {
+    let response = {
+        success: false,
+        url,
+    };
+
+    try {
+        const { host, pathname } = new URL(url);
+
+        if (host !== "www.instagram.com") {
+            return response;
+        }
+
+        let shortCode = pathname.trim().split("/")[2];
+
+        if (shortCode?.length === 11) {
+            return {
+                url: url,
+                shortCode,
+                success: true,
+            };
+        }
+
+        return response;
+    } catch (error) {
+        log("error in isValid : ", error);
+        log("caused by : ", url);
+        return response;
+    }
+};
+
 module.exports = {
     waitFor,
     findMediaByShortCode,
+    isValidInstaUrl,
 };
