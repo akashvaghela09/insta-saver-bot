13b3c0d7c4d78da291e76e4c1cc4df02eb1086a4
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-06-28 19:48:03 +0530

    Use fastdl for queue

--- src/queue.js
+++ src/queue.js
@@ -1,23 +1,31 @@
-const { fetchOwnerId, getStreamDataRecursively } = require("./apis");
 const { REQUEST_STATUS, MESSSAGE } = require("./constants");
 const ContentRequest = require("./models/ContentRequest");
-const ContentResponse = require("./models/ContentResponse");
-const { log } = require("./utils");
+const { log, waitFor } = require("./utils");
 
-const {
-    sendRequestedData,
-    sendMessage,
-    deleteMessages,
-} = require("./telegramActions");
+const { sendRequestedData } = require("./telegramActions");
+const { Browser } = require("./config");
+const { scrapWithFastDl } = require("./apis");
 
 let queue = [];
 let processing = false;
+const QUEUE_LIMIT = 15; // Maximum number of items in the queue
+
+const logPendingCount = async () => {
+    // Count remaining pending requests
+    const pendingCount = await ContentRequest.countDocuments({
+        status: REQUEST_STATUS.PENDING,
+        retryCount: { $lt: 5 },
+    });
+    log("Remaining items in queue:", pendingCount);
+};
 
 // Process the queue of content requests
 const processQueue = async () => {
     log("processQueue run -----------------");
     if (processing || queue.length === 0) {
         log("queue stopped processing ----------------");
+        log("processing ", processing);
+        log("queue length ", queue.length);
         return;
     }
 
@@ -25,84 +33,27 @@ const processQueue = async () => {
     const job = queue.shift();
     log("job to process: ", job);
 
-    // Check if the job's retry count exceeds the limit
-    if (job.retryCount >= 5 || job.retryCount === -1) {
-        log(
-            `Skipping job ${job.id} because retry count ${job.retryCount} exceeds the limit.`
-        );
-        processing = false;
-        processQueue(); // Process the next job in the queue
-        return;
-    }
-
     try {
-        let messagesToDelete = [];
+        if (!Browser.browserInstance) {
+            console.log("seems like browser was closed");
+            await Browser.Open();
+        }
+
+        let result = await scrapWithFastDl(job.requestUrl);
 
         log(MESSSAGE.DOWNLOADING.replace("requestUrl", job.requestUrl));
 
-        // if (job.retryCount === 0) {
-        //     // Send message indicating content gathering
-        //     const downloadingMessage = await sendMessage({
-        //         ...job,
-        //         message: MESSSAGE.GATHERING_CONTENT,
-        //     });
-
-        //     if (downloadingMessage) {
-        //         messagesToDelete.push(downloadingMessage.message_id);
-        //     }
-        // }
-
-        let ownerIdResponse = await fetchOwnerId(job.shortCode);
-        const ownerId = ownerIdResponse.data;
-        console.log("ownerIdResponse: ", ownerIdResponse);
-
-        if (!ownerId) {
-            await ContentRequest.findByIdAndUpdate(
-                job.id,
-                {
-                    $set: { updatedAt: new Date() },
-                    $inc: { retryCount: 1 },
-                },
-                { new: true }
-            );
-
-            job.retryCount += 1;
-            return;
-        }
-
-        // Retrieve content data from the specified URL
-        const result = await getStreamDataRecursively(job.shortCode, ownerId);
-        log("result: ", result);
-
-        // Delete temporary messages after content retrieval
-        await deleteMessages({ ...job, messagesToDelete });
-
         if (!result.success) {
-            // Update request status on failure
-            const updatedRequest = await ContentRequest.findByIdAndUpdate(
-                job.id,
-                {
-                    $set: { updatedAt: new Date() },
-                    $inc: { retryCount: 1 },
-                },
-                { new: true }
-            );
+            console.log("failed the scrap request");
+            let retryCount = job.retryCount + 1;
+            let newStatus =
+                retryCount < 5 ? REQUEST_STATUS.PENDING : REQUEST_STATUS.FAILED;
 
-            if (!updatedRequest) {
-                log(`Content request with ID ${job.id} not found.`);
-                // Handle the case where the document with job.id is not found
-                return;
-            }
-
-            job.retryCount += 1;
-        } else {
-            // Update request status on success and save response data
             await ContentRequest.findByIdAndUpdate(job.id, {
-                status: REQUEST_STATUS.DONE,
-                updatedAt: new Date(),
-                retryCount: job.retryCount + 1,
+                $set: { updatedAt: new Date(), status: newStatus },
+                $inc: { retryCount: job.retryCount + 1 },
             });
-
+        } else {
             const newResponseData = new ContentResponse({
                 owner: { ...result.data?.owner },
                 requestedBy: { ...job?.requestedBy },
@@ -120,21 +71,28 @@ const processQueue = async () => {
 
             await newResponseData.save();
 
+            await waitFor(500);
+
             // Send requested data to the user
             await sendRequestedData({ ...result.data, ...job });
 
-            // Count remaining pending requests
-            const pendingCount = await ContentRequest.countDocuments({
-                status: REQUEST_STATUS.PENDING,
+            // Update request status on success and save response data
+            await ContentRequest.findByIdAndUpdate(job.id, {
+                status: REQUEST_STATUS.DONE,
+                updatedAt: new Date(),
+                retryCount: job.retryCount + 1,
             });
-            log("Remaining items in queue:", pendingCount);
+
+            logPendingCount();
         }
     } catch (error) {
         log("Error processing job:", error);
     } finally {
         processing = false;
         log("process next item");
-        processQueue(); // Process the next job in the queue
+
+        await waitFor(500);
+        await processQueue(); // Process the next job in the queue
     }
 };
 
@@ -154,7 +112,13 @@ const fetchPendingRequests = async () => {
         const pendingRequests = await ContentRequest.find({
             status: REQUEST_STATUS.PENDING,
             retryCount: { $lt: 5 },
-        }).limit(15);
+            requestedBy: {
+                userName: "akashvaghela09",
+                firstName: "Akash",
+            },
+        })
+            .sort({ requestedAt: 1 })
+            .limit(QUEUE_LIMIT);
         log("Fetched pending requests: ", pendingRequests.length);
 
         // Clear the current queue
@@ -173,6 +137,10 @@ const fetchPendingRequests = async () => {
         });
 
         log("Queue updated with fresh pending requests.", queue.length);
+        logPendingCount();
+
+        await waitFor(500);
+        await processQueue();
     } catch (error) {
         log("Error fetching pending requests:", error);
     }
@@ -192,20 +160,24 @@ const initQueue = async () => {
                     "got new request ========================================="
                 );
                 const newRequest = change.fullDocument;
-                addToQueue({
-                    id: newRequest._id.toString(),
-                    shortCode: newRequest.shortCode,
-                    requestUrl: newRequest.requestUrl,
-                    requestedBy: newRequest.requestedBy,
-                    retryCount: newRequest.retryCount,
-                    chatId: newRequest.chatId,
-                });
+
+                // Only add request if queue is empty, otherwise wait for queue to complete 
+                if (queue.length === 0) {
+                    addToQueue({
+                        id: newRequest._id.toString(),
+                        shortCode: newRequest.shortCode,
+                        requestUrl: newRequest.requestUrl,
+                        requestedBy: newRequest.requestedBy,
+                        retryCount: newRequest.retryCount,
+                        chatId: newRequest.chatId,
+                    });
+                }
                 log("New request added to the queue:", newRequest._id);
             }
         });
 
         // Periodically synchronize the queue with the database
-        setInterval(fetchPendingRequests, 60000); // Adjust the interval as needed
+        setInterval(fetchPendingRequests, 30000); // Adjust the interval as needed
 
         log("process queue after fetching pending request");
         await processQueue();
@@ -215,4 +187,4 @@ const initQueue = async () => {
 };
 
 // Export functions for adding to the queue and initializing it
-module.exports = { addToQueue, initQueue };
+module.exports = { initQueue };
